bec7b5baf3642a2546545717eaf33c7acd226744
CoderDream <coderdream@gmail.com>
2019-09-15 21:48:11 +0800

    Green - example 1 201909152148

--- Chapter01/FirstDemo/FirstDemo/ViewController.swift
+++ Chapter01/FirstDemo/FirstDemo/ViewController.swift
@@ -37,6 +37,8 @@ class ViewController: UIViewController {
 //            $0 + (vowels.contains($1) ? 1 : 0)
 //        }
 //    }
-    
+    func makeHeadline(from string: String) -> String {
+        return "This Is A Test Headline"
+    }
 }
 
