45d455a75474e511ddbd32462227232c1bed1d8a
CoderDream <coderdream@gmail.com>
2019-09-18 17:18:02 +0800

    测试表格视图行数2 201909181717

--- Chapter04/ToDo/ToDo/ItemListDataProvider.swift
+++ Chapter04/ToDo/ToDo/ItemListDataProvider.swift
@@ -14,7 +14,31 @@ class ItemListDataProvider: NSObject, UITableViewDataSource, UITableViewDelegate
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
         //return 0
-        return itemManager?.toDoCount ?? 0
+        //return itemManager?.toDoCount ?? 0
+//        let numberOfRows: Int
+//        switch section {
+//        case 0:
+//            numberOfRows = itemManager?.toDoCount ?? 0
+//        case 1:
+//            //numberOfRows = 1
+//            numberOfRows = itemManager?.doneCount ?? 0
+//        default:
+//            numberOfRows = 0
+//        }
+//        return numberOfRows
+        
+        guard let itemManager = itemManager else { return 0 }
+        guard let itemSection = Section(rawValue: section) else {
+            fatalError()
+        }
+        let numberOfRows: Int
+        switch itemSection {
+        case .toDo:
+            numberOfRows = itemManager.toDoCount
+        case .done:
+            numberOfRows = itemManager.doneCount
+        }
+        return numberOfRows
     }
     
     func tableView(_ tableView: UITableView,
@@ -26,4 +50,9 @@ class ItemListDataProvider: NSObject, UITableViewDataSource, UITableViewDelegate
         in tableView: UITableView) -> Int {
         return 2
     }
+}
+
+enum Section: Int {
+    case toDo
+    case done
 }
--- Chapter04/ToDo/ToDoTests/ItemListDataProviderTests.swift
+++ Chapter04/ToDo/ToDoTests/ItemListDataProviderTests.swift
@@ -24,16 +24,29 @@ class ItemListDataProviderTests: XCTestCase {
     override func tearDown() {
     }
     
+    // 表格视图包含两个小节
     func test_NumberOfSections_IsTwo() {
         let numberOfSections = tableView.numberOfSections
         XCTAssertEqual(numberOfSections, 2)
     }
     
+    // 测试表格视图行数
     func test_NumberOfRows_Section1_IsToDoCount() {
         sut.itemManager?.add(ToDoItem(title: "Foo"))
         XCTAssertEqual(tableView.numberOfRows(inSection: 0), 1)
         sut.itemManager?.add(ToDoItem(title: "Bar"))
         tableView.reloadData()
         XCTAssertEqual(tableView.numberOfRows(inSection: 0), 2)
+    }
+    
+    // 测试表格视图行数2
+    func test_NumberOfRows_Section2_IsToDoneCount() {
+        sut.itemManager?.add(ToDoItem(title: "Foo"))
+        sut.itemManager?.add(ToDoItem(title: "Bar"))
+        sut.itemManager?.checkItem(at: 0)
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 1)
+        sut.itemManager?.checkItem(at: 0)
+        tableView.reloadData()
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 2)
     }
 }
